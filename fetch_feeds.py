26c69dd845576f4e63c6a972b8ef1029ca2d54d8
Jonathan Maietta <jonathanmaietta@gmail.com>
2025-10-22 09:45:15 -0400

    Update fetch_feeds.py

--- fetch_feeds.py
+++ fetch_feeds.py
@@ -16,6 +16,7 @@ RSS_FEEDS = [
     {"name": "Sherdog", "url": "https://sherdog.com/rss/news.xml"},
     {"name": "MMA Weekly YouTube", "url": "https://www.youtube.com/feeds/videos.xml?channel_id=UC7tdFNggH_jwjJTDWqYelmA"},
     {"name": "MMA Junkie YouTube", "url": "https://www.youtube.com/feeds/videos.xml?channel_id=UCxQfUu6vIJGZDODSwhr0m9w"},
+    {"name": "Chael Sonnen YouTube", "url": "https://www.youtube.com/feeds/videos.xml?channel_id=UCRlvF4jIeBWqXJDGNXfPyVw"},
     {"name": "ESPN MMA YouTube", "url": "https://www.youtube.com/feeds/videos.xml?channel_id=UCO4AcsPKEkIqDmbeiZLfd1A"}
 ]
 
